8df04d6c98ed659bb8ad0523050338f954fc10c6
ohunyk <ohunyk@gmail.com>
2020-05-05 18:16:38 +0900

    Add goroutine tutorial and url checker

---
+++ learngo/goroutine/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	// time.Sleep(time.Second * 10)
+	// result := <-c
+	// fmt.Println(result)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + " is sexy"
+}
