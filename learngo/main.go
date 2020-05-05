0d8d3267179f1bcb8ba3856c28708b75a8e241d4
ohunyk <ohunyk@gmail.com>
2020-05-05 18:39:43 +0900

    init scrapper project

--- learngo/main.go
+++ learngo/main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/ohunyk/learngo/mydict"
+	"net/http"
 )
 
+type result struct {
+	url    string
+	status string
+}
+
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	dictionary := mydict.Dictionary{}
+	var results = make(map[string]string)
+	// var results = map[string]string{}
 
-	baseword := "hello"
+	c := make(chan result)
 
-	dictionary.Add(baseword, "First")
-	dictionary.Search(baseword)
-	dictionary.Delete(baseword)
+	urls := []string{
+		"https://www.airbbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
 
-	word, err := dictionary.Search(baseword)
-	if err != nil {
-		fmt.Println(err)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		res := <-c
+		results[res.url] = res.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- result) {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		c <- result{url: url, status: "FAILED"}
 	} else {
-		fmt.Println(word)
+		c <- result{url: url, status: "OK"}
 	}
 }
